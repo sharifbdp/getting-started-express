86376f907ec741e9ebc570bfe39b7fa20b2da7a3
Sharif <sharifbdp@gmail.com>
2016-02-13 04:20:15 +0600

    test some route

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login Here' });
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Login Here'});
+});
+
+router.get('/reg', function (req, res, next) {
+    res.render('variable', {
+        layout: false,
+        title: 'Basic Variable',
+        user: {
+            firstname: 'Sharif',
+            lastname: 'Ul Islam',
+            email: 'sharif@test.com'
+        }
+    }
+    );
+    
+//    data = {
+//        title: 'Reg Here',
+//        user: {
+//            firstname: 'Sharif',
+//            lastname: 'Ul Islam',
+//            email: 'sharif@test.com'
+//        }
+//    };
+//    res.render('variable', {layout: false, data: data});
+    
 });
 
 module.exports = router;
